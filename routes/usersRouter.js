606289eb095f98bba4dff26f0738b9e535a8e361
Karina <milkokarina13@gmail.com>
2024-10-14 19:31:34 +0300

    Add user controller

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,4 +1,5 @@
 const { Router } = require("express");
+const { usersController } = require("../controllers");
 
 // аналогічно передбачити роутер для /api/tasks
 // /api/users
@@ -6,16 +7,14 @@ const usersRouter = Router();
 
 usersRouter
   .route("/")
-  .post(() => {})
-  .get((req, res) => {
-    res.status(501).send("Not Implemented");
-  });
+  .post(usersController.createUser)
+  .get(usersController.getUsers);
 
 usersRouter
   .route("/:userId")
-  .get(() => {})
-  .patch(() => {})
-  .delete(() => {});
+  .get(usersController.getUserById)
+  .patch(usersController.updateUserById)
+  .delete(usersController.deleteUserById);
 
 // usersRouter.post('/', () => {});
 // usersRouter.get('/', (req, res) => {
