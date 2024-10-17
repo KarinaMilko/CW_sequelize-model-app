58967636864bc8592745e1047ae0a9259bbc92cf
Karina <milkokarina13@gmail.com>
2024-10-17 21:11:18 +0200

    Add model,migration Task

---
+++ migrations/20241017185705-create-task.js
@@ -0,0 +1,37 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("tasks", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        type: Sequelize.STRING(50),
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING(200),
+        allowNull: true,
+      },
+      completed: {
+        type: Sequelize.ENUM("completed", "not completed"),
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("tasks");
+  },
+};
