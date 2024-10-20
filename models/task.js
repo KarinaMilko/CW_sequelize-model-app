5867ec2da10f12408a1c039982223f34b979a8a0
Karina <milkokarina13@gmail.com>
2024-10-20 16:01:00 +0300

    Add migration,seed tasks

--- models/task.js
+++ models/task.js
@@ -1,5 +1,7 @@
-"use strict";
-const { Model, Op } = require("sequelize");
+'use strict';
+const {
+  Model
+} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Task extends Model {
     /**
@@ -11,51 +13,12 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  Task.init(
-    {
-      title: {
-        type: DataTypes.STRING(50),
-        allowNull: false,
-        validate: {
-          len: [2, 50],
-        },
-      },
-      description: {
-        type: DataTypes.STRING(200),
-        allowNull: true,
-        validate: {
-          len: [2, 200],
-        },
-      },
-      completed: {
-        type: DataTypes.ENUM("completed", "not completed"),
-        allowNull: false,
-
-        validate: {
-          isIn: [["completed", "not completed"]],
-        },
-      },
-      deadline: {
-        type: DataTypes.DATEONLY,
-        validate: {
-          isDate: true,
-          isAfter: new Date().toISOString(),
-        },
-      },
-      userId: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: "Users",
-          key: "id",
-        },
-        allowNull: true,
-      },
-    },
-    {
-      sequelize,
-      modelName: "Task",
-      underscored: true,
-    }
-  );
+  Task.init({
+    body: DataTypes.STRING,
+    deadline: DataTypes.DATEONLY
+  }, {
+    sequelize,
+    modelName: 'Task',
+  });
   return Task;
 };
